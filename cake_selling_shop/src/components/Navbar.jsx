117b9bd51c7d3cb3477bf35ea819bbe6860712c6
NguyenDuy <suytdocotdien@gmail.com>
2024-07-13 09:38:00 +0700

    Update

--- cake_selling_shop/src/components/Navbar.jsx
+++ cake_selling_shop/src/components/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
                     </button>
                     <div className="collapse navbar-collapse bg-white" id="navbarCollapse">
                         <div className="navbar-nav mx-auto">
-                            <a href="index.html" className="nav-item nav-link">Trang chủ</a>
-                            <a href="shop.html" className="nav-item nav-link">Sản phẩm</a>
+                            <a href="home" className="nav-item nav-link">Trang chủ</a>
+                            <a href="products" className="nav-item nav-link">Sản phẩm</a>
                             <div className="nav-item dropdown">
                                 <a href="#" className="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Pages</a>
                                 <div className="dropdown-menu m-0 bg-secondary rounded-0">
@@ -35,11 +35,11 @@ const Navbar = () => {
                                     <a href="404.html" className="dropdown-item active">404 Page</a>
                                 </div>
                             </div>
-                            <a href="contact.html" className="nav-item nav-link">Contact</a>
+                            <a href="contact" className="nav-item nav-link">Liên hệ</a>
                         </div>
                         <div className="d-flex m-3 me-0">
                             <button className="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i className="fas fa-search text-primary"></i></button>
-                            <a href="#" className="position-relative me-4 my-auto">
+                            <a href="cart" className="position-relative me-4 my-auto">
                                 <i className="fa fa-shopping-bag fa-2x"></i>
                                 <span className="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style={{ top: '-5px', left: '15px', height: '20px', minWidth: '20px' }}>3</span>
                             </a>
